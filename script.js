9714391a5d484984b8ec01c431e9335b513c895d
Benjy0812 <108727379+Benjy0812@users.noreply.github.com>
2025-04-17 23:04:53 +0200

    Add theme toggle functionality with dark mode support

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 document.getElementById("lucky").addEventListener("click", function () {
   window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
 });
+
+const themeToggle = document.getElementById("theme-toggle");
+const themeImg = document.getElementById("themeImg")
+const body = document.body;
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+});
+
+themeToggle.addEventListener("click", () => {
+  if (body.classList.contains("dark-mode")) {
+    themeImg.src = "moon.png";
+  } else {
+    themeImg.src = "sun.png";
+  }
+})
